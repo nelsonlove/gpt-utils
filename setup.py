c16d4ad303f91b8485d48515040557ca168d01db
nelsonlove <nelson.love@me.com>
2021-08-13 03:30:04 -0400

    Update .gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gpt_utils',
-    version='0.0.7.2',
+    version='0.8.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[
